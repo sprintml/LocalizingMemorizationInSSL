6db32673827a0df58c6d1647893f2aabec9d4646
w1995 <wenhao.wang@cispa.de>
2024-10-18 12:41:09 +0200

    update

--- UnitMem.py
+++ UnitMem.py
@@ -41,13 +41,13 @@ model.load_state_dict(torch.load('./model_weights.pth'))
 
 new_m = torchvision.models._utils.IntermediateLayerGetter(model,{'layer3_residual2': 'feat1'})
 
-final = []
+final1 = []
 
 
 
 if __name__ == '__main__':
     for img, label in tqdm(iter(dataloader)):
-        final1 = []
+        final = []
         for j in range(10):
             out = new_m(Aug(img))
             for k, v in out.items():
